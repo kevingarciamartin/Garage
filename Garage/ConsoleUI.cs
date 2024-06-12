ec3b34c8ad06d5db36d32f60ebde669e0f9d7366
Kevin Garcia Martin <kevingm@live.se>
2024-06-12 12:10:51 +0200

    Add application exit logic

--- Garage/ConsoleUI.cs
+++ Garage/ConsoleUI.cs
@@ -1,6 +1,43 @@
-﻿namespace Garage
+﻿
+
+
+
+
+namespace Garage
 {
     internal class ConsoleUI
     {
+        internal static void Clear()
+        {
+            Console.Clear();
+        }
+
+        internal static void ConfirmExit()
+        {
+            WriteLine("Are you sure you want to exit the application?"
+                + "\n1. Yes."
+                + "\n0. No.");
+        }
+
+        internal static object GetKey() => Console.ReadKey(intercept: true).Key;
+
+        internal static void PrintMenu()
+        {
+            WriteLine("Main Menu"
+                + "\n1. List all parked vehicles."
+                + "\n2. List the amount of each vehicle type currently in the garage."
+                + "\n3. Add a vehicle to the garage."
+                + "\n4. Remove a vehicle from the garage."
+                + "\n5. Search a specific vehicle by registration number."
+                + "\n6. Search vehicles by one or more characteristics."
+                + "\n0. Exit the application.");
+        }
+
+        internal static void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine();
+        }
+        
     }
 }
--- Garage/Manager.cs
+++ Garage/Manager.cs
@@ -1,25 +1,59 @@
 ﻿
 
+
+
 namespace Garage
 {
     internal class Manager
     {
+        internal void Initialize()
+        {
+            GarageHandler garageHandler = new();
+        }
+
         internal void Run()
         {
             bool applicationInProgress = true;
 
             do
             {
-                //PrintMenu
-
-                //GetCommand()
+                PrintMenu();
+                GetCommand();
             } while (applicationInProgress);
         }
 
-        internal void Initialize()
+        private void GetCommand()
         {
-            ConsoleUI ui = new();
-            GarageHandler garageHandler = new();
+            var keyPressed = ConsoleUI.GetKey();
+
+            switch (keyPressed)
+            {
+                case ConsoleKey.D0:
+                    ConsoleUI.ConfirmExit();
+
+                    var confirmationKeyPressed = ConsoleUI.GetKey();
+
+                    switch (confirmationKeyPressed)
+                    {
+                        case ConsoleKey.D1:
+                            Environment.Exit(0);
+                            break;
+                        case ConsoleKey.D0:
+                            break;
+                        default:
+                            ConsoleUI.WriteLine("Please enter a valid input.");
+                            break;
+                    }
+                    break;
+                default:
+                    ConsoleUI.WriteLine("Please enter a valid input.");
+                    break;
+            }
+        }
+
+        private void PrintMenu()
+        {
+            ConsoleUI.PrintMenu();
         }
     }
 }
