1ccc39942eb8f7681853d85ab4f92ec5c9db11a2
Kevin Garcia Martin <kevingm@live.se>
2024-06-12 12:44:49 +0200

    Add Test folder

---
+++ Garage.Tests/Garage.Manager.Tests/ManagerTests.cs
@@ -0,0 +1,11 @@
+namespace Garage.Tests.Garage.Manager.Tests
+{
+    public class ManagerTests
+    {
+        [Fact]
+        public void ConfirmExitCommand_ShouldExitApp_WhenKeyPressedIs0()
+        {
+
+        }
+    }
+}
