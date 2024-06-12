5f8735e3770afa6bbeddb97d867bafac7763ac8a
Kevin Garcia Martin <kevingm@live.se>
2024-06-12 10:46:37 +0200

    Add project files.

---
+++ Garage/Manager.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Garage
+{
+    internal class Manager
+    {
+        internal void Run()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            ConsoleUI ui = new();
+            GarageHandler garageHandler = new();
+        }
+    }
+}
