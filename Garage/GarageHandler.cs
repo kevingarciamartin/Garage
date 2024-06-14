f8af489e7dc7161caaa04d679f25d7acb853fad0
Kevin Garcia Martin <kevingm@live.se>
2024-06-14 10:48:40 +0200

    Uncouple menu

--- Garage/GarageHandler.cs
+++ Garage/GarageHandler.cs
@@ -11,47 +11,6 @@ namespace Garage
         {
             GarageCapacity = garageCapacity;
             _garage = new(GarageCapacity);
-
-            PrintMenu();
-        }
-
-        private void PrintMenu()
-        {
-            bool inGarage = true;
-            do
-            {
-                ConsoleUI.PrintMenu();
-
-                var keyPressed = ConsoleUI.GetKey();
-
-                switch (keyPressed)
-                {
-                    case ConsoleKey.D1:
-                        //Todo: List all parked vehicles
-                        break;
-                    case ConsoleKey.D2:
-                        //Todo: List the amount of each vehicle type currently in the garage
-                        break;
-                    case ConsoleKey.D3:
-                        //Todo: Add a vehicle to the garage
-                        break;
-                    case ConsoleKey.D4:
-                        //Todo: Remove a vehicle from the garage
-                        break;
-                    case ConsoleKey.D5:
-                        //Todo: Search a specific vehicle by registration number
-                        break;
-                    case ConsoleKey.D6:
-                        //Todo: Search vehicles by one or more characteristics
-                        break;
-                    case ConsoleKey.D0:
-                        inGarage = false;
-                        break;
-                    default:
-                        ConsoleUI.ErrorMessage("Please enter a valid input.");
-                        break;
-                }
-            } while (inGarage);
         }
     }
 }
