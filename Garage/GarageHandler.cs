0e2a1af382da97ca591b12c31386075638050f8f
Kevin Garcia Martin <kevingm@live.se>
2024-06-18 14:55:10 +0200

    Add properties and Print

--- Garage/GarageHandler.cs
+++ Garage/GarageHandler.cs
@@ -6,10 +6,26 @@ namespace Garage
     internal class GarageHandler
     {
         public List<Garage<Vehicle>> Garages;
+        public static int Capacity => 5;
+        public int Count => Garages.Count;
+        public bool IsFull => Count >= Capacity;
+        public bool IsEmpty => Count <= 0;
 
         public GarageHandler()
         {
             Garages = new();
+        }
+
+        public void Print()
+        {
+            int num = 1;
+
+            foreach (var garage in Garages)
+            {
+                Console.WriteLine($"{num++}. {garage.Name} ({garage.Count}/{garage.Capacity})");
+            }
+
+            Console.WriteLine();
         }
 
         public void Add(string garageName, int garageCapacity)
