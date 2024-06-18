8a19e8b735617b27956b81dc2a0365f8d22abfc0
Kevin Garcia Martin <kevingm@live.se>
2024-06-18 09:53:57 +0200

    Add Colors

---
+++ Garage/Vehicles/Properties/Colors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Vehicles.Properties
+{
+    internal class Colors
+    {
+        public const string White = "White";
+        public const string Black = "Black";
+        public const string Gray = "Gray";
+        public const string Red = "Red";
+        public const string Blue = "Blue";
+        public const string Yellow = "Yellow";
+        public const string Green = "Green";
+        public const string Purple = "Purple";
+        public const string Orange = "Orange";
+        public const string Brown = "Brown";
+        public const string Pink = "Pink";
+        public const string Cyan = "Cyan";
+        public const string Magenta = "Magenta";
+        public const string Gold = "Gold";
+        public const string Silver = "Silver";
+        public const string Maroon = "Maroon";
+        public const string Turquoise = "Turquoise";
+        public const string Beige = "Beige";
+        public const string Violet = "Violet";
+
+        public static string[] AllColors = [White, Black, Gray, Red, Blue, Yellow, Green, Purple, Orange, Brown, Green, Turquoise, Violet];
+    }
+}
